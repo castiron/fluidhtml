9398057952ad23f11fab8a2ca3a060a0c49f7856
Kyle Pratuch <kyle@castironcoding.com>
2020-10-19 11:46:10 -0700

    [B] Fix T3 v9 compat issue

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$_EXTKEY] =
-	'EXT:' . $_EXTKEY . '/Classes/Hook/CmsLayout.php:CIC\Fluidhtml\Hook\PageLayoutViewDrawItemHook';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$_EXTKEY.'_pi1'] =
+	CIC\Fluidhtml\Hook\PageLayoutViewDrawItemHook::class;
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	'CIC.'.$_EXTKEY, 'pi1',
--- ext_tables.php
+++ ext_tables.php
@@ -5,10 +5,11 @@ if (!defined('TYPO3_MODE')) {
 
 $TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] = $TCA['tt_content']['types']['html']['showitem'];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
-	'LLL:EXT:fluidhtml/locallang_db.xml:tt_content.CType_pi1',
-	$_EXTKEY . '_pi1',
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.gif'
-),'CType');
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'_pi1',
+	'Fluid HTML',
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'ext_icon.gif'
+);
 
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,'static/fluid_html_static_template/', 'Fluid HTML Static Template');
